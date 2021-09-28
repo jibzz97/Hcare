e4212bf0522013778a868e5a97e01e2aace9af04
adnanmerrakchi <adnanmerrakchi@gmail.com>
2021-09-28 22:59:38 +0100

    fix chartjs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,121 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {Component, ViewChild } from "@angular/core";
-import {
-  ApexChart,
-  ApexAxisChartSeries,
-  ChartComponent,
-  ApexDataLabels,
-  ApexPlotOptions,
-  ApexYAxis,
-  ApexLegend,
-  ApexGrid
-} from "ng-apexcharts";
-
-type ApexXAxis = {
-  type?: "category" | "datetime" | "numeric";
-  categories?: any;
-  labels?: {
-    style?: {
-      colors?: string | string[];
-      fontSize?: string;
-    };
-  };
-};
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  dataLabels: ApexDataLabels;
-  plotOptions: ApexPlotOptions;
-  yaxis: ApexYAxis;
-  xaxis: ApexXAxis;
-  grid: ApexGrid;
-  colors: string[];
-  legend: ApexLegend;
-};
-@Component({
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ApexTitleSubtitle, ApexChart, ApexAxisChartSeries } from "ng-apexcharts";
+@Component({ 
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  title: ApexTitleSubtitle;
+  chart: ApexChart;
+  series: ApexAxisChartSeries;
   
-  @ViewChild("chart") chart: ChartComponent | undefined;
-  public chartOptions: Partial<ChartOptions>;
-
- 
   constructor() {
-    this.chartOptions = {
-      series: [
-        {
-          name: "distibuted",
-          data: [21, 22, 10, 28, 16, 21, 13, 30]
-        }
-      ],
-      chart: {
-        height: 350,
-        type: "bar",
-        events: {
-          click: function(chart, w, e) {
-            // console.log(chart, w, e)
-          }
-        }
-      },
-      colors: [
-        "#008FFB",
-        "#00E396",
-        "#FEB019",
-        "#FF4560",
-        "#775DD0",
-        "#546E7A",
-        "#26a69a",
-        "#D10CE8"
-      ],
-      plotOptions: {
-        bar: {
-          columnWidth: "45%",
-          distributed: true
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      legend: {
-        show: false
-      },
-      grid: {
-        show: false
-      },
-      xaxis: {
-        categories: [
-          ["John", "Doe"],
-          ["Joe", "Smith"],
-          ["Jake", "Williams"],
-          "Amber",
-          ["Peter", "Brown"],
-          ["Mary", "Evans"],
-          ["David", "Wilson"],
-          ["Lily", "Roberts"]
-        ],
-        labels: {
-          style: {
-            colors: [
-              "#008FFB",
-              "#00E396",
-              "#FEB019",
-              "#FF4560",
-              "#775DD0",
-              "#546E7A",
-              "#26a69a",
-              "#D10CE8"
-            ],
-            fontSize: "12px"
-          }
-        }
+    this.title = {text: 'hello'};
+    this.chart = {  width: 400,
+                    height: 400,
+                    type: "line"};
+    this.series =  [
+      {
+        name: "My-series",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
       }
-    };
-   }
-
-  ngOnInit(): void {
+    ];
   }
+
+  ngOnInit(): void {}
  
 }
