4e309495824e5ea13d621301200bcdf5e91d995f
ahmed <jibzz@gmail.com>
2021-11-14 21:17:05 +0100

    last_update_of_Hcare

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+ 
+  loginForm = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+
+  });
+     
+    private email : any;
+    private password :any;
+  get f()
+  {
+    return this.loginForm.controls;
+  }
+
+  constructor()  {}
+  
+    ngOnInit(): void {
+     
+    }
+
+    
+  
+    
+  
+    
+  
+   
+    login() {
+      this.email=this.loginForm.get('email');
+      this.password=this.loginForm.get('password');
+      this.loginForm;
+      console.log(this.loginForm);
+      if (this.loginForm.valid) {
+        console.log('form submitted');
+      } else {
+        this.validateAllFormFields(this.loginForm);
+      }
+    }
+  
+    validateAllFormFields(formGroup: FormGroup) {
+      Object.keys(formGroup.controls).forEach(field => {
+        console.log(field);
+        const control = formGroup.get(field);
+        if (control instanceof FormControl) {
+          control.markAsTouched({ onlySelf: true });
+        } else if (control instanceof FormGroup) {
+          this.validateAllFormFields(control);
+        }
+      });
+
+
+
+  }
+  
+ 
+    
+  
+    
+}
